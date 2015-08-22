226ebdebabc1c5255cd7ab8918a9e3c24402d477
eduardo ortega <eduardo.ortega102@hotmail.com>
2015-08-22 12:22:19 +0100

    Added CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private Vector3 distanceToPlayer;
+	[SerializeField]
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		this.distanceToPlayer = this.transform.position - this.player.transform.position;
+	}
+	
+
+	void LateUpdate () {
+		this.transform.position = this.player.transform.position + this.distanceToPlayer;
+	}
+}

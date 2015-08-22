1c8896a779aaab6a9a2edd657eea39c6b062ade6
ClubbingSealCub <danroe@witty.com>
2015-08-22 13:09:01 +0100

    no message

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private int score;
+    private int euphoria;
+    private bool euphoric;
+
+	void Start () {
+	
+	}
+
+    void Update () {
+	
+	}
+
+    void addScore(int amount) {
+        score += amount;
+    }
+
+    void takeDamage(int amount) {
+        if (euphoric) {
+            euphoria -= amount;
+        }
+        else {
+            //Llamada a muerte
+        }
+    }
+}
+
+

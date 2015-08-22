4bf7d553c921719af56e64dbf503269d8b6d3a5e
Josue Gonzalez Barrera <josu.vicio@gmail.com>
2015-08-22 11:26:07 +0100

    Added NPCBehaviour

---
+++ Assets/Scripts/NPCBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class NPCBehaviour : MonoBehaviour {
+    protected Transform playerPosition;
+    protected bool playerOnRage;
+}

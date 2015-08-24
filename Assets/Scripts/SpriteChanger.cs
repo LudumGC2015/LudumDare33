c34a03b0d98b053515a581537f2f3322c0d7f9a0
ClubbingSealCub <danroe@witty.com>
2015-08-24 17:22:33 +0100

    rotacion implementada

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -4,17 +4,40 @@ using System.Collections;
 public class SpriteChanger : MonoBehaviour {
 
     public Sprite[] sprites;
+    private EuphoriaController euphoriaC;
+    private Transform playerTransform;
     SpriteRenderer spriteR;
     void Awake() {
         spriteR = GetComponent<SpriteRenderer>();
+        euphoriaC = GetComponentInParent<EuphoriaController>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    public void GoEuphoric() {
-        spriteR.sprite = sprites[1];
-    }
 
-    public void ChillOut() {
-        spriteR.sprite = sprites[0];
-    }
+    public void Update()
+    {
+        if (euphoriaC.IsEuphoric() == false)
+        {
+            if (playerTransform.rotation.eulerAngles.z >= 225 && playerTransform.rotation.eulerAngles.z <= 315)
+            {
+                spriteR.sprite = sprites[0];
+                Debug.Log("Miro abajo");
+            }
+            else if (playerTransform.rotation.eulerAngles.z < 225 && playerTransform.rotation.eulerAngles.z >= 135)
+            {
+                spriteR.sprite = sprites[2];
+            }
+            else if (playerTransform.rotation.eulerAngles.z < 135 && playerTransform.rotation.eulerAngles.z >= 45)
+            {
+                spriteR.sprite = sprites[3];
+            }
+            else spriteR.sprite = sprites[1];
+            Debug.Log(playerTransform.rotation.eulerAngles.z);
+        }
+        else if (playerTransform.rotation.eulerAngles.z > 180)
+        {
+            spriteR.sprite = sprites[4];
+        } else spriteR.sprite = sprites[5];
+    } 
 }
